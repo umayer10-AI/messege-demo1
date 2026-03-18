691350ea7fd4851707b0d4143fd2d83735bc58a1
umayer10-AI <mdumayer10@gmail.com>
2026-03-18 16:37:09 +0600

    add

--- src/I1.jsx
+++ src/I1.jsx
@@ -4,7 +4,7 @@ const I1 = ({i,t}) => {
     
     return (
         <div className='w-[70%] border p-4 rounded-2xl bg-yellow-400 text-black h-fit'>
-            <h2 className='text-xl font-bold text-green-900'>Umayer: {i || "Umayer"}</h2>
+            <h2 className='text-xl font-bold text-green-900'>title: {i || "Umayer"}</h2>
             <p><span className='text-xl font-bold'>Text: </span>{t || "Assalamualaikum guys, How are you all I am fine Thank you so much Visit my website"}</p>
         </div>
     );
